42f9e03b1c3a9a65ad7ed67f30ac7e9aed1147fd
Cyrus822 <52565847+t217145@users.noreply.github.com>
2024-10-26 22:05:09 +0800

    Add ShadowSocks

--- terraform-azure-free-vm/main.tf
+++ terraform-azure-free-vm/main.tf
@@ -77,7 +77,29 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     destination_port_range     = var.vpn_port_number
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-  }   
+  }
+  security_rule {
+    name                       = "ShadowSocksU"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = var.shadowsocks_port
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "ShadowSocksT"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = var.shadowsocks_port
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
