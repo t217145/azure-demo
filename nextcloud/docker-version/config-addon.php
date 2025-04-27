addebf5be572e28b50bfb469323d591c7afc4dd7
Cyrus Cheng <cyrus@cyrus822.com>
2025-04-27 14:49:54 +0800

    Add docker version of NextCloud

---
+++ nextcloud/docker-version/config-addon.php
@@ -0,0 +1,11 @@
+  'overwrite.cli.url' => 'https://your-domain/nextcloud',
+  'overwriteprotocol' => 'https',
+  'overwritehost' => 'your-domain',
+  'overwritewebroot' => '/nextcloud'
+  'trusted_proxies' => ['127.0.0.1', '::1'],
+  
+  
+  'trusted_domains' =>
+  array (
+    0 => 'appfevers.com',
+  ),
