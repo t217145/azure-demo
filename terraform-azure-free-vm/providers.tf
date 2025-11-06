446b89539b96c74bdef02c4ae69849b0df6e64f0
Cyrus822 <52565847+t217145@users.noreply.github.com>
2025-11-06 22:57:01 +0800

    Update the project to adopt Terraform Cloud

--- terraform-azure-free-vm/providers.tf
+++ terraform-azure-free-vm/providers.tf
@@ -4,19 +4,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~> 3.100"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    # tls = {
-    #   source = "hashicorp/tls"
-    #   version = "~>4.0"
-    # }
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc        = true
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
